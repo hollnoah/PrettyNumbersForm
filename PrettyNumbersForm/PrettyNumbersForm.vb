dafd009a5d7a4ba51e6c782e6a306d9ae81011ea
Noah Holloway <noahholloway23@gmail.com>
2025-10-02 10:00:23 -0600

    add notes

--- PrettyNumbersForm/PrettyNumbersForm.vb
+++ PrettyNumbersForm/PrettyNumbersForm.vb
@@ -1,12 +1,13 @@
-﻿Public Class PrettyNumbersForm
+﻿'Add engineering notation with metric prefixes
+Public Class PrettyNumbersForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
 
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
-        If InputTextBox.Text <> "" And IsNumeric(InputTextBox.text) Then
+        If InputTextBox.Text <> "" And IsNumeric(InputTextBox.Text) Then
             'get text from textbox1 and format in scientific notation using tostring method
-            InputTextBox.Text = Convert.ToDouble(InputTextBox.Text).ToString("E2")
+            InputTextBox.Text = CDec(InputTextBox.Text).ToString("E2")
         End If
     End Sub
 End Class
